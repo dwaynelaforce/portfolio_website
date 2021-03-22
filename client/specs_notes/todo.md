d47721addf35146007a52ec51a3395ee96487550
Dwayne LaForce <dwayne.laforce@gmail.com>
2021-03-22 16:52:47 -0700

    contact form works

--- client/specs_notes/todo.md
+++ client/specs_notes/todo.md
@@ -1,9 +1,16 @@
-# P1
+# To do
+
+## P1
+Contact is a form rather than just displaying my info
 About Me section written
 Projects figured out
-Contact is a form rather than just displaying my info (means setting up server backend)
 
-## Done
+## P2
+Add readme
+include instructions about emailSettings.js needing to be created
+
+# Done
+Nodemailer backend is built!
 Menu links are responsive somehow (mvp would be use A tags)
 get user IP address and display
 store last visit in local storage and display
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,12 +1,52 @@
+import axios from 'axios'
+import {useState} from 'react'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+
 export default function Contact(){
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    
+    function submitHandler(e) {
+        e.preventDefault();
+        const userData = {name, email, message};
+        axios.post('http://localhost:8000/api', userData)
+            .then(res =>{
+                window.alert("Email sent successfully");
+                console.log(res);
+            })
+            .catch(err=>{
+                window.alert("There was an issue sending the email");
+                console.log(err);
+            });
+    }
+
     return(
-        <div>
-            <h4>Contact Info</h4>
-            <ul>
-                <li>dwayne.laforce@gmail.com</li>
-                <li>linkedin.com/in/dwayne-laforce-0690a165</li>
-                <li>github.com/dwaynelaforce</li>
-            </ul>
-        </div>
+        <Form onSubmit={submitHandler}>
+            <h4>Send me a message</h4>
+            <p>(pls dont break my legs)</p>
+            <Row>
+                <Form.Group as={Col} lg>
+                    <Form.Control name="name" type="text" placeholder="Your name" 
+                    onChange={(e)=>setName(e.target.value)}
+                    required/>
+                </Form.Group>
+                <Form.Group as={Col} lg>
+                    <Form.Control name="email" type="email" placeholder="Your email" 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    required/>
+                </Form.Group>
+            </Row>
+            <Form.Group>
+                <Form.Control name="message" as="textarea" rows={3} placeholder="Write your message to me here" 
+                onChange={(e)=>setMessage(e.target.value)}
+                required/>
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+        </Form>
     );
 }
